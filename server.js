822acc9f80ebe610a7177f97a6dbc0b43b014312
piradite <veis2k15veis@gmail.com>
2024-08-07 19:02:55 +0200

    djwi

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const generateValidHtml = () => `
         <input type="hidden" name="_captcha" value="false">
         <input type="hidden" name="_template" value="table">
         <input type="text" name="_honey" style="display:none">
-        <button onClick="$.fn.fancybox.close()" type="submit" class="button">Send</button>
+        <button onClick="$.fancybox.close()" type="submit" class="button">Send</button>
       </form>
     </div>
   </body>
